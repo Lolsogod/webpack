6bc6c4e0625e08a68a69c75122961ea4543601eb
DIO <46161640+Lolsogod@users.noreply.github.com>
2023-11-09 21:11:43 +0400

    fix css

--- src/test.tsx
+++ src/test.tsx
@@ -1,9 +1,9 @@
 import smile from "./images/smile.png"
 import css from "./styles/main.css"
 
-console.log(css)
+
 const Test = () => (
-  <div className="cont">
+  <div className="cont" style={css}>
     <h1>Hello World!</h1>;
     <img src={smile} alt="смайлик" />
   </div>
